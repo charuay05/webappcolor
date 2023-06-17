864bab430210fc2c7ed6cf2fb4af0eb776e6218e
charuay05 <57581355+charuay05@users.noreply.github.com>
2023-06-18 01:31:59 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -23,16 +23,10 @@ color = os.environ.get('APP_COLOR') or random.choice(["red","green","blue","blue
 def main():
     #return 'Hello'
     print(color)
-    #return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
-    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
+    return render_template('hello.html', name=socket.gethostname(), contents="tttttt", color=color_codes[color])
+    
 
 
-@app.route('/read_file')
-def read_file():
-    f = open("/data/testfile.txt")
-    contents = f.read()
-    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
-    #return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8082")
