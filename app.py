bf0b868d4ff3381368523ac822c6a14353f5f0fe
charuay05 <57581355+charuay05@users.noreply.github.com>
2023-06-18 02:04:58 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ def main():
 @app.route('/read_file')
 def read_file():
     f = open("/data/testfile.txt", "r")
-    contents = f.read()
-    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
+    #contents = f.read()
+    return render_template('hello.html', name=socket.gethostname(), contents=f.read(), color=color_codes[color])
     #return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
 
