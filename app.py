9dce7658c03a20d529aeba1843a54de68f81a96a
charuay05 <57581355+charuay05@users.noreply.github.com>
2023-06-18 01:21:47 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -24,6 +24,12 @@ def read_file():
     f = open("/data/testfile.txt")
     contents = f.read()
     return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
+
+@app.route("/")
+def main():
+    #return 'Hello'
+    print(color)
+    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8082")
